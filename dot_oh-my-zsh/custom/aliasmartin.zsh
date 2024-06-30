93c429eaf1f4dab6a5ec795f368f12f645cf1bb5
martin35 <op6666@gmail.com>
2024-06-29 20:06:37 -0400

    update aliasmartin and vifmrc

--- dot_oh-my-zsh/custom/aliasmartin.zsh
+++ dot_oh-my-zsh/custom/aliasmartin.zsh
@@ -7,7 +7,7 @@ alias ca='chezmoi apply'
 alias iinstall='paru -S'
 alias iremove='sudo paru -R'
 alias mc='sudo mc'
-alias vim="nvim"
+alias vim="NVIM_APPNAME=mvim nvim"
 alias mvim="NVIM_APPNAME=mvim nvim"
 alias ls="lsd -a"
 alias tree="exa -T"
